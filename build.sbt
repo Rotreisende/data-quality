11bfc4a1863d6e10c49b2b194b690248b3a17cde
Fisenko <sfisenko@neoflex.ru>
2021-08-19 14:15:37 +0400

    add tailrec read

--- build.sbt
+++ build.sbt
@@ -3,3 +3,5 @@ name := "csv-quality"
 version := "0.1"
 
 scalaVersion := "2.13.6"
+
+libraryDependencies += "com.github.tototoshi" %% "scala-csv" % "1.3.8"
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,29 +1,43 @@
+import com.github.tototoshi.csv.CSVReader
+
+import java.io.File
+import scala.annotation.tailrec
 import scala.collection.mutable
 import scala.io.Source
 
 object Main extends App {
-  type Row = mutable.Map[String, Either[Option[String], String]]
+  type Row = mutable.Map[String, String]
 
-  val source = Source.fromFile("src/main/resources/data.csv")
-  var dataSets = materialize[String](source.getLines())
+  var dataSets = materialize(read("src/main/resources/data.csv"))
 
-  val rows = dataSets.map(item => {
-    item.split(",") match {
-      case _@Array(surname, name, part) =>
-        val row = mutable.Map[String, Either[Option[String], String]]()
-        row("surname") = Right(surname)
-        row("name") = Right(name)
-        row("part") = Left(Some(part))
-        row
+  val rows = dataSets.map {
+    case _@Seq(surname, name, part) =>
+      val row = mutable.Map[String, String]()
+      row("surname") = surname
+      row("name") = name
+      row("part") = part
+      row
 
-      case _ => None
-    }
-  })
+    case _ => None
+  }
+
   println(rows)
 
-  def materialize[T](it: Iterator[T]): List[T] = {
-    it.foldRight(List.empty[T]) {
-      (accumulator, elem) => accumulator :: elem
+  def read(root: String) = {
+    val reader = CSVReader.open(new File(root))
+    val iterator = reader.iterator
+    iterator
+  }
+
+  def materialize(iterator: Iterator[Seq[String]]): List[Seq[String]] = {
+    @tailrec
+    def loop(iterator: Iterator[Seq[String]], accumulator: List[Seq[String]]): List[Seq[String]] = {
+      if (iterator.hasNext) {
+        loop(iterator, iterator.next() :: accumulator)
+      } else {
+        accumulator
+      }
     }
+    loop(iterator, List.empty).reverse
   }
 }
